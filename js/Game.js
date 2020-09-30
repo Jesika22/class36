779b874b2647709d16d1de4caa72a1b937a2c972
Jesika22 <jesikikk@gmail.com>
2020-09-29 19:52:57 -0700

    multiple playerGame

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,48 @@ class Game {
     });
   }
 
-  start(){
+ async start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playerCountref = await database.ref("playerCount").once("value");
+      if(playerCountref.exists()){
+        playerCount = playerCountref.val();
+        player.getCount();
+      }
+      
       form = new Form()
       form.display();
+    }
+  }
+
+  play(){
+
+    form.hide();
+    textSize(40);
+    fill("black");
+    text("gameStarts",250,100);
+    Player.getPlayerInfo();
+    if(allPlayers !== undefined){
+       var displayPosition = 130;
+    for(var i in allPlayers){ 
+      if(i === "player"+player.index){
+        fill("red");
+
+      }
+      else{
+        fill("black");
+      }
+
+      displayPosition+=20;
+      textSize(20);
+      text(allPlayers[i].name+": "+allPlayers[i].distance,120,displayPosition);
+
+    }
+    }
+   
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance += 50;
+      player.update();
     }
   }
 }
